594fde43d988ae7880461e15b47a541723958be9
Kevin Havn <kevin-havn@hotmail.com>
2023-11-10 19:17:53 +0100

    updated login test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,45 +1,48 @@
 import { login } from "./login";
 
-    const email = "unit.test@stud.noroff.no";
-    const password = "unit.test.password123"
-    const accessToken = "mockToken"
-
-
-global.localStorage = mockLocalStorage
+const name = "unittester"
+const email = "unit.test@stud.noroff.no";
+const password = "unittest1";
+const accessToken = "mockToken";
 
 const mockLocalStorage = {
     getItem: jest.fn(),
     setItem: jest.fn(),
     removeItem: jest.fn(),
     clear: jest.fn(),
-};
+  };
+  global.localStorage = mockLocalStorage;
 
-const mockFailedApiFetch = jest.fn().mockResolvedValue({
-    ok: false,
-    statusText: "Unauthorized",
-    statusCode: 401,
-})
+const apiFetchMock = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue({
+    name: name,
+    email: email,
+    accessToken: accessToken,
+  }),
+});
 
-describe("Login function", () => {
-    it('should send credentials to API and receive accesstoken to insert to localstorage', async () => {
-        global.fetch = mockFetchApi;
-        await login(email,password);
-        expect(fetch).toBe(mockFetchApi);
-        expect(mockLocalStorage.setItem).toHaveBeenCalledWith("token", "mockToken")
-        expect(mockLocalStorage.setItem).toHaveBeenCalledWith("profile", '{\"name\":\"tester\",\"email\":\"unit.test@stud.noroff.no\"}');
-    })
-})
+const apiFailMock = jest.fn().mockResolvedValue({
+  ok: false,
+  statusText: "Unauthorized",
+  statusCode: 401,
+});
 
-it("should fail if api call does not succeed in putting token into storage", async () => {
-    global.fetch = mockFailedApiFetch;
-    await expect(login(email,password)),rejects.toThrow("Unauthorized");
-})
+describe("login function", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
 
-const mockFetchApi = jest.fn().mockResolvedValue({
-    ok: true,
-    json: jest.fn().mockResolvedValue({
-        name:"testUser",
-        email: email,
-        accessToken: accessToken
-    })
+  it("should attempt an API call, and insert bearer token to localstorage", async () => {
+    global.fetch = apiFetchMock;
+    await login(email, password);
+    expect(fetch).toBe(apiFetchMock);
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith("token", '\"mockToken\"');
+    expect(mockLocalStorage.setItem).toHaveBeenCalledWith("profile", '{\"name\":\"unittester\",\"email\":\"unit.test@stud.noroff.no\"}');
+  });
+  
+  it("should not pass if the API call fails or login information is unexpected", async () => {
+    global.fetch = apiFailMock;
+    await expect(login(email, password)).rejects.toThrow("Unauthorized");
+  });
 });
